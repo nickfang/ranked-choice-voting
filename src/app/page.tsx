356636b9b55d448dcd5798f42bd6606aea49c33b
Nick Fang <fang.nicholas@gmail.com>
2023-11-26 03:20:52 -0600

    Adding mui.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import Image from 'next/image'
 import styles from './page.module.css'
 
