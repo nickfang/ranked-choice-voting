d7fbaed5a39aa861ca6aa5de2d6ee4dbf91023f5
Nick Fang <fang.nicholas@gmail.com>
2023-11-28 14:54:03 -0600

    created test data with chatgpt.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/votes', function(req, res, next) {
+  res.send({votes: [{id: 1, name: 'test', vote: ['test1', 'test2']}]})
+})
+
 module.exports = router;
